e9c8ece915aa5e3b72785eae8bbdca9de29e6698
Matt Kubilus <mattkubilus@gmail.com>
2022-12-24 17:14:08 -0500

    Beware of static creation

--- autoortho/test_downloader.py
+++ autoortho/test_downloader.py
@@ -7,13 +7,14 @@ import pytest
 import platform
 
 import downloader
-
+downloader.TESTMODE = True
 
 def test_setup(tmpdir):
     pass
 
 def test_list(tmpdir):
     d = downloader.Downloader(os.path.join(tmpdir, 'Custom Scenery'))
+    d.region_list = ['test']
     d.find_releases()
     assert d.regions != {}
 
@@ -21,20 +22,21 @@ def test_fetch(tmpdir):
     scenery_dir = os.path.join(tmpdir, 'Custom Scenery')
     dl_dir = os.path.join(tmpdir, 'downloads')
     d = downloader.Downloader(scenery_dir, dl_dir)
+
     d.region_list = ['test']
     d.find_releases()
     assert d.regions != {}
 
-    na = d.regions.get('test')
-    assert len(na.ortho_urls) == 1
-    assert len(na.overlay_urls) == 1
+    r = d.regions.get('test')
+    assert len(r.ortho_urls) == 1
+    assert len(r.overlay_urls) == 1
     
-    d.download_region('test')
+    r.download()
     downloads = os.listdir(dl_dir)
     downloads.sort()
     assert downloads == ['y_test_overlays.zip.00', 'z_test_00.zip']
 
-    d.extract('test')
+    r.extract()
     extracts = os.listdir(scenery_dir)
     extracts.sort()
     assert extracts == [
@@ -50,4 +52,3 @@ def test_fetch(tmpdir):
     else:
         assert orthodetails == ['_textures', 'test_info.json', 'textures']
         assert os.path.islink(os.path.join(scenery_dir, "z_test_00", "textures"))
-
