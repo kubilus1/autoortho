b96bb8604e198d3188c0ac10d2cd270d5e971070
Matt Kubilus <mattkubilus@gmail.com>
2023-02-09 22:28:45 -0500

    libjpeg

--- buildreqs.sh
+++ buildreqs.sh
@@ -5,7 +5,7 @@ apt-get install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
 apt-get update
 apt-get install -y make curl patchelf python3.10 python3.10-tk zlib1g-dev \
-    ccache python3.10-distutils python3.10-dev
+    ccache python3.10-distutils python3.10-dev libjpeg-dev
 
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 
