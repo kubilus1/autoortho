51c46b4154367e593fbdba50da8185a54c2913c7
Matt Kubilus <mattkubilus@gmail.com>
2023-02-09 22:12:26 -0500

    add buildreqs

---
+++ buildreqs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+apt-get install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt-get update
+apt-get install -y make curl patchelf python3.10 python3.10-tk zlib1g-dev \
+    ccache python3.10-distutils python3.10-dev
+
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+
+python3.10 -m pip install -U pip
+python3.10 -m pip install -r requirements-build.txt
+python3.10 -m pip install -r requirements.txt
