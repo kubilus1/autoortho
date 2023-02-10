b9c68314468196c77dbf65a5b417c3f4291ede55
Matt Kubilus <mattkubilus@gmail.com>
2023-02-09 22:37:43 -0500

    Install setuptools

--- buildreqs.sh
+++ buildreqs.sh
@@ -10,5 +10,6 @@ apt-get install -y make curl patchelf python3.10 python3.10-tk zlib1g-dev \
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 
 python3.10 -m pip install -U pip
+python3.10 -m pip install setuptools
 python3.10 -m pip install -r requirements-build.txt
 python3.10 -m pip install -r requirements.txt
