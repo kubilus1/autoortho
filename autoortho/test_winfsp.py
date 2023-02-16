29cbf6446e67ecedd78181b60cb1070177340663
Holger Teutsch <holger.a.teutsch@gmail.com>
2023-02-16 16:33:32 +0100

    add more test cases

--- autoortho/test_winfsp.py
+++ autoortho/test_winfsp.py
@@ -12,9 +12,14 @@ print(os.path.exists(dir + "/textures/water_transition.png"))
 print(os.path.exists(dir + "/textures/21552_34032_BI16.dds"))
 
 # non existent
-print(os.path.exists(dir + "/textures/bad.dds"))
+#print(os.path.exists(dir + "/textures/bad.dds"))
 
 fh = open(dir + "/textures/21552_34032_BI16.dds", "rb")
+print(fh)
+data = fh.read(1024)
+print(len(data))
+
+fh = open(dir + "/textures/21584_34176_BI16.dds", "rb")
 print(fh)
 data = fh.read(1024)
 print(len(data))
