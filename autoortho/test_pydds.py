7c89ef443eb6a509ca5bccf0d44f8fce87282553
Matt Kubilus <mattkubilus@gmail.com>
2023-07-15 15:15:20 -0400

    fix test

--- autoortho/test_pydds.py
+++ autoortho/test_pydds.py
@@ -8,7 +8,7 @@ from . import pydds
 
 import pytest
 #from PIL import Image
-from aoimage import AoImage as Image
+from .aoimage import AoImage as Image
 #TESTPNG=os.path.join('testfiles', 'test_tile.png')
 TESTJPG=os.path.join('testfiles', 'test_tile2.jpg')
 
