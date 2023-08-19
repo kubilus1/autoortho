8e359801ab8ec6c38c4579ecbf993d06ddc0b215
Matt Kubilus <mattkubilus@gmail.com>
2023-08-19 12:03:00 -0400

    Fix log handler being overridden

--- autoortho/__main__.py
+++ autoortho/__main__.py
@@ -11,9 +11,10 @@ def setuplogs():
     if not os.path.isdir(log_dir):
         os.makedirs(log_dir)
 
+    log_level=logging.DEBUG if os.environ.get('AO_DEBUG') or CFG.general.debug else logging.INFO
     logging.basicConfig(
             #filename=os.path.join(log_dir, "autoortho.log"),
-            level=logging.DEBUG if os.environ.get('AO_DEBUG') or CFG.general.debug else logging.INFO,
+            level=log_level,
             handlers=[
                 #logging.FileHandler(filename=os.path.join(log_dir, "autoortho.log")),
                 logging.handlers.RotatingFileHandler(
@@ -24,6 +25,8 @@ def setuplogs():
                 logging.StreamHandler()
             ]
     )
+    log = logging.getLogger(__name__)
+    log.info(f"Setup logs: {log_dir}, level: {log_level}")
 
 import autoortho
 
